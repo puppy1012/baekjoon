c9279666c579fbe79984a4cd067ca82f05c777a9
Seunghyun Lee <puppy1012@naver.com>
2023-03-01 23:48:54 +0900

    단순출력

---
+++ 23235.py
@@ -0,0 +1,10 @@
+case_num = 0
+
+while True:
+    if input()=='0':
+
+        break
+    else:
+        case_num+=1
+        fmt=f"Case {case_num}: Sorting... done!"
+        print(fmt)
--- 5341.py
+++ 5341.py
@@ -4,3 +4,9 @@ while True:
     result=0
     for i in range(1,n+1): result+=i
     print(result)
+
+'''
+result=0
+for i in range(1,(int(input()))+1) : result+=i
+print(result)
+'''
